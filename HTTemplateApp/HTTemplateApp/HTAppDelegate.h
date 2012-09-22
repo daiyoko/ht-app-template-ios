5a64eae81491b1cbbbb5557c272ebafd5df03e76
daiyoko <trytry291-2112@yahoo.com.hk>
2012-09-22 15:12:02 +0800

    20120922151159

---
+++ HTTemplateApp/HTTemplateApp/HTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HTAppDelegate.h
+//  HTTemplateApp
+//
+//  Created by kazuya on 22/9/12.
+//  Copyright (c) 2012 kazuya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HTTemplateApp/HTTemplateApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HTTemplateApp
+//
+//  Created by kazuya on 22/9/12.
+//  Copyright (c) 2012 kazuya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HTAppDelegate class]));
+    }
+}

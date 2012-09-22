5baeee6dc7ebfb8e020eb943909c94bc8070e8fb
daiyoko <trytry291-2112@yahoo.com.hk>
2012-09-22 16:03:02 +0800

    Add Language Plist

--- HTTemplateApp/HTTemplateApp/HTAppDelegate.m
+++ HTTemplateApp/HTTemplateApp/HTAppDelegate.m
@@ -16,6 +16,9 @@
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+    
+    NSLog(@"AppInternalLabel: %@", NSLocalizedStringFromTable(@"AppInternalLabel", @"InfoPlist", nil));
+    
     return YES;
 }
 
